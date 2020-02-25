39790d4f61e9a246da1c04d94d121edb9b38a740
UntungBagusPurnomo <untungbaguspurnomo30@gmail.com>
2020-02-25 16:51:34 +0700

    ConstantHelper_duplicate_assetid

--- src/main/java/id/io/asset/controller/AssetController.java
+++ src/main/java/id/io/asset/controller/AssetController.java
@@ -9,6 +9,9 @@ import id.io.asset.model.AssetModel;
 import id.io.asset.util.constant.ConstantHelper;
 import id.io.asset.util.database.AssetDatabaseHelper;
 import id.io.asset.util.database.UUIDGeneratorHelper;
+import java.awt.HeadlessException;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.UUID;
 import org.apache.http.HttpStatus;
@@ -16,7 +19,7 @@ import org.json.JSONObject;
 
 /**
  *
- * @author user
+ * @author untung bagus purnomo
  */
 public class AssetController extends BaseController{
     
@@ -33,57 +36,57 @@ public class AssetController extends BaseController{
         return assetList;
     }
     
-   
     public AssetModel getAsset(String assetId) {
         AssetModel asset = assetDbHelper.findById(assetId);
         return asset;
     }
-    
     //create
-    
     public JSONObject create(JSONObject json) {
         JSONObject response = new JSONObject();
         if (json.length() != 0) {
-            UUID uuid = uuidGenerator.generateUUID(json.getString("assetcode"));
+            UUID uuid = uuidGenerator.generateUUID(json.getString(ConstantHelper.ASSET_ASSETCODE));
+            boolean codeValidate = assetDbHelper.assetCodeValidity(json.getString(ConstantHelper.ASSET_ASSETCODE));
+            if (codeValidate) {
+                AssetModel model = new AssetModel();
+                model.setAssetid(uuid.toString());
+                model.setAssetcode(json.getString(ConstantHelper.ASSET_ASSETCODE));
+                model.setAssetname(json.getString(ConstantHelper.ASSET_ASSETNAME));
+                model.setTypeid(json.getString(ConstantHelper.ASSET_TYPEID));
+                model.setManufacture(json.getString(ConstantHelper.ASSET_MANUFACTURE));
+                model.setModel(json.getString(ConstantHelper.ASSET_MODEL));
+                model.setVendorid(json.getString(ConstantHelper.ASSET_VENDORID));
+                model.setNote(json.getString(ConstantHelper.ASSET_NOTE));
 
-            AssetModel model = new AssetModel();
-            model.setAssetid(uuid.toString());
-            model.setAssetcode(json.getString("assetcode"));
-            model.setAssetname(json.getString("assetname"));
-            model.setTypeid(json.getString("typeid"));
-            model.setManufacture(json.getString("manufacture"));
-            model.setModel(json.getString("model"));
-            model.setVendorid(json.getString("vendorid"));
-            model.setNote(json.getString("note"));
-            
-            assetDbHelper.create(model);
+                assetDbHelper.create(model);
 
-            response.put(ConstantHelper.HTTP_CODE, HttpStatus.SC_OK);
-            response.put(ConstantHelper.HTTP_REASON, "create_asset_successful");
-            response.put(ConstantHelper.HTTP_MESSAGE, "Create Asset Successful!");
+                response.put(ConstantHelper.HTTP_CODE, HttpStatus.SC_OK);
+                response.put(ConstantHelper.HTTP_REASON, "create_asset_successful");
+                response.put(ConstantHelper.HTTP_MESSAGE, "Create Asset Successful!");
+            } else {
+                response.put(ConstantHelper.HTTP_CODE, HttpStatus.SC_BAD_REQUEST);
+                response.put(ConstantHelper.HTTP_REASON, "duplicate_assetid");
+                response.put(ConstantHelper.HTTP_MESSAGE, "Error Duplicate AssetId " + codeValidate);
+            }
         } else {
-            response.put(ConstantHelper.HTTP_CODE, HttpStatus.SC_BAD_REQUEST);
+           response.put(ConstantHelper.HTTP_CODE, HttpStatus.SC_BAD_REQUEST);
             response.put(ConstantHelper.HTTP_REASON, "error_create_asset");
             response.put(ConstantHelper.HTTP_MESSAGE, "Error Create Asset : No such Asset");
         }
         return response;
     }
-    
     //update
-    
     public JSONObject update(String assetId, JSONObject json) {
         JSONObject response = new JSONObject();
         if (json.length() != 0) {
-
             AssetModel model = new AssetModel();
-            model.setAssetcode(json.getString("assetcode"));
-            model.setAssetname(json.getString("assetname"));
-            model.setTypeid(json.getString("typeid"));
-            model.setManufacture(json.getString("manufacture"));
-            model.setVendorid(json.getString("vendorid"));
-            model.setNote(json.getString("note"));
+            model.setAssetcode(json.getString(ConstantHelper.ASSET_ASSETCODE));
+            model.setAssetname(json.getString(ConstantHelper.ASSET_ASSETNAME));
+            model.setTypeid(json.getString(ConstantHelper.ASSET_TYPEID));
+            model.setManufacture(json.getString(ConstantHelper.ASSET_MANUFACTURE));
+            model.setModel(json.getString(ConstantHelper.ASSET_MODEL));
+            model.setVendorid(json.getString(ConstantHelper.ASSET_VENDORID));
+            model.setNote(json.getString(ConstantHelper.ASSET_NOTE));
             assetDbHelper.update(assetId, model);
-
             response.put(ConstantHelper.HTTP_CODE, HttpStatus.SC_OK);
             response.put(ConstantHelper.HTTP_REASON, "update_asset_successful");
             response.put(ConstantHelper.HTTP_MESSAGE, "Update Asset Successful!");
@@ -95,7 +98,6 @@ public class AssetController extends BaseController{
         return response;
     }
     //delete
-    
     public JSONObject delete(String assetId) {
         JSONObject json = new JSONObject();
         int result = assetDbHelper.delete(assetId);
@@ -103,7 +105,6 @@ public class AssetController extends BaseController{
             json.put(ConstantHelper.HTTP_CODE, HttpStatus.SC_OK);
             json.put(ConstantHelper.HTTP_REASON, "delete_asset_successful");
             json.put(ConstantHelper.HTTP_MESSAGE, "Delete Asset Successful!");
-
         } else {
             json.put(ConstantHelper.HTTP_CODE, HttpStatus.SC_BAD_REQUEST);
             json.put(ConstantHelper.HTTP_REASON, "error_delete_asset");
