755d8711be8e382ad6d3533cc85b310c0fc66ded
untung bagus purnomo <untungbaguspurnomo30>
2020-02-25 20:23:38 +0700

    ChangeLicenseHeader_IllegalStateMapper

--- src/main/java/id/io/asset/util/rest/mapper/IllegalStateMapper.java
+++ src/main/java/id/io/asset/util/rest/mapper/IllegalStateMapper.java
@@ -1,9 +1,22 @@
-package id.io.asset.util.rest.mapper;
+/**
+  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+  *
+  * Copyright (c) 2019 IO-Teknologi Indonesia, and individual contributors
+  * as indicated by the @author tags. All Rights Reserved
+  *
+  * The contents of this file are subject to the terms of the
+  * Common Development and Distribution License (the License).
+  *
+  * Everyone is permitted to copy and distribute verbatim copies
+  * of this license document, but changing it is not allowed.
+  *
+  */
+package id.io.asset.util.rest.mapper; 
 
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.Response.Status; 
 import javax.ws.rs.ext.ExceptionMapper;
-import javax.ws.rs.ext.Provider;
+import javax.ws.rs.ext.Provider; 
 import id.io.asset.util.rest.model.ServiceResponse;
 
 @Provider
