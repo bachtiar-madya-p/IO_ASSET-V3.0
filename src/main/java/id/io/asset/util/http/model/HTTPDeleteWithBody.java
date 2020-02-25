085edc30102a5a287ccc210e753cfd4e3fc6af4b
untung bagus purnomo <untungbaguspurnomo30>
2020-02-25 20:10:55 +0700

    ChangeLicenseHeader_HTTPDeleteWithBody

--- src/main/java/id/io/asset/util/http/model/HTTPDeleteWithBody.java
+++ src/main/java/id/io/asset/util/http/model/HTTPDeleteWithBody.java
@@ -1,6 +1,19 @@
-package id.io.asset.util.http.model;
+/**
+  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+  *
+  * Copyright (c) 2019 IO-Teknologi Indonesia, and individual contributors
+  * as indicated by the @author tags. All Rights Reserved
+  *
+  * The contents of this file are subject to the terms of the
+  * Common Development and Distribution License (the License).
+  *
+  * Everyone is permitted to copy and distribute verbatim copies
+  * of this license document, but changing it is not allowed.
+  *
+  */
+package id.io.asset.util.http.model; 
 
-import java.net.URI;
+import java.net.URI; 
 
 import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
 
