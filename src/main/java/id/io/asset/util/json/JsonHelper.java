bb303af03478061571166e135bb46c2e4350f396
untung bagus purnomo <untungbaguspurnomo30>
2020-02-25 20:15:47 +0700

    ChangeLicenseHeader_JsonHelper

--- src/main/java/id/io/asset/util/json/JsonHelper.java
+++ src/main/java/id/io/asset/util/json/JsonHelper.java
@@ -1,7 +1,20 @@
-package id.io.asset.util.json;
+/**
+  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+  *
+  * Copyright (c) 2019 IO-Teknologi Indonesia, and individual contributors
+  * as indicated by the @author tags. All Rights Reserved
+  *
+  * The contents of this file are subject to the terms of the
+  * Common Development and Distribution License (the License).
+  *
+  * Everyone is permitted to copy and distribute verbatim copies
+  * of this license document, but changing it is not allowed.
+  *
+  */
+package id.io.asset.util.json; 
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.ArrayList; 
 import java.util.Arrays;
 import java.util.List;
 import com.fasterxml.jackson.annotation.JsonInclude;
