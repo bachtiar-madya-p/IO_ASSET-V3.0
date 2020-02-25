6adcb6d718a3ed2cadd22bde47a828ee76f3eecb
untung bagus purnomo <untungbaguspurnomo30>
2020-02-25 20:17:35 +0700

    ChangeLicenseHeader_AppLogger

--- src/main/java/id/io/asset/util/log/AppLogger.java
+++ src/main/java/id/io/asset/util/log/AppLogger.java
@@ -1,4 +1,17 @@
-package id.io.asset.util.log;
+/**
+  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+  *
+  * Copyright (c) 2019 IO-Teknologi Indonesia, and individual contributors
+  * as indicated by the @author tags. All Rights Reserved
+  *
+  * The contents of this file are subject to the terms of the
+  * Common Development and Distribution License (the License).
+  *
+  * Everyone is permitted to copy and distribute verbatim copies
+  * of this license document, but changing it is not allowed.
+  *
+  */
+package id.io.asset.util.log; 
 
 import id.io.asset.util.json.JsonHelper;
 
