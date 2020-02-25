f50c1aded01fa4b62e680257a3a1f1638a01b0aa
untung bagus purnomo <untungbaguspurnomo30>
2020-02-25 20:09:02 +0700

    ChangeLicenseHeader_HTTPConnectionManager

--- src/main/java/id/io/asset/util/http/HTTPConnectionManager.java
+++ src/main/java/id/io/asset/util/http/HTTPConnectionManager.java
@@ -1,6 +1,19 @@
-package id.io.asset.util.http;
+/**
+  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+  *
+  * Copyright (c) 2019 IO-Teknologi Indonesia, and individual contributors
+  * as indicated by the @author tags. All Rights Reserved
+  *
+  * The contents of this file are subject to the terms of the
+  * Common Development and Distribution License (the License).
+  *
+  * Everyone is permitted to copy and distribute verbatim copies
+  * of this license document, but changing it is not allowed.
+  *
+  */
+package id.io.asset.util.http; 
 
-import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLContext; 
 import org.apache.http.config.RegistryBuilder;
 import org.apache.http.conn.socket.ConnectionSocketFactory;
 import org.apache.http.conn.socket.PlainConnectionSocketFactory;
