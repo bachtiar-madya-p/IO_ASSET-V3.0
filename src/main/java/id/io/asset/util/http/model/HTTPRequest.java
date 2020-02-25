6f4e44141b660deb82a11eeaf17ccffe0ce28d7b
untung bagus purnomo <untungbaguspurnomo30>
2020-02-25 20:14:46 +0700

    ChangeLicenseHeader_HTTPRequest

--- src/main/java/id/io/asset/util/http/model/HTTPRequest.java
+++ src/main/java/id/io/asset/util/http/model/HTTPRequest.java
@@ -1,3 +1,16 @@
+/**
+  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+  *
+  * Copyright (c) 2019 IO-Teknologi Indonesia, and individual contributors
+  * as indicated by the @author tags. All Rights Reserved
+  *
+  * The contents of this file are subject to the terms of the
+  * Common Development and Distribution License (the License).
+  *
+  * Everyone is permitted to copy and distribute verbatim copies
+  * of this license document, but changing it is not allowed.
+  *
+  */
 package id.io.asset.util.http.model;
 
 import java.util.ArrayList;
