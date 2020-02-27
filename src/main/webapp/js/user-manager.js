4629b6860502b8819e914a65e76936788d9e7a54
Muhammad Zaahid <muhammadzaahid24@gmail.com>
2020-02-27 20:35:12 +0700

    get json data

--- src/main/webapp/js/user-manager.js
+++ src/main/webapp/js/user-manager.js
@@ -3,35 +3,59 @@ function hsRestUrl() {
 }
 
 var tableData={};
-
-$(document).ready(function () {
-    $("#userManagerDT").dataTable();
-    $.ajax({
-        url: hsRestUrl() + "/user",
-        type: 'get',
-        dataType: 'json',
-        success: function (result) {
-            let daftar = result;
-            var html = '';
-
-            $.each(daftar, function (i, data) {
-                html +=
-                        `<tr>           <td></td>
-                                        <td> ` + data.membername + `</td>
-                                        <td>` + data.username + `</td>
-                                        <td>` + data.email + `</td>
-                                        <td>` + data.levelid + `</td>
-                                        <td> ` + data.departmentid + ` </td>
-                                        <td> ` + data.isactive + ` </td>
-                                        <td></td>
-                                    </tr>`;
-
-                //This is selector of my <tbody> in my table
-                $("#list-list").html(html);
-
+ $(document).ready(function(){
+                
+                $.getJSON(hsRestUrl() + "/user",function(data){
+                    var tab_data = '';                     
+                    $.each(data, function(key,value){
+                        
+                        tab_data += '<tr>';
+                            tab_data += '<td></td>';
+                            tab_data += '<td>'+value.membername+'</td>';
+                            tab_data += '<td>'+value.username+'</td>';
+                            tab_data += '<td>'+value.email+'</td>';
+                            tab_data += '<td>'+value.levelid+'</td>';
+                            tab_data += '<td>'+value.departmentid+'</td>';
+                            tab_data += '<td><span class="label label-info">'+value.isadmin+'</span></td>';                                                      
+                            tab_data += '<td ><div class="btn-group"><a href="javascript:void(0)" data-toggle="tooltip" title="Edit" class="btn btn-xs btn-default"><i class="fa fa-pencil"></i></a><a href="javascript:void(0)" data-toggle="tooltip" title="Delete" class="btn btn-xs btn-danger"><i class="fa fa-times"></i></a></div></td>';                    
+                        tab_data += '</tr>';
+                    });
+                    $('#userManagerDT').append(tab_data).DataTable({ responsive: true});
+                });
             });
-            console.log(daftar);
-        }
-    });
-})
-
+//$(document).ready(function () {
+//
+//    const  columns = daftar;
+//
+//     $('#userManagerDT').DataTable({
+//                 "ajax" : { url: hsRestUrl() + "/user",
+//                          type: 'get',
+//                          dataType: 'json',
+//                          success: function (result) {
+//                               daftar = result;      
+//                               console.log(daftar);
+//                              }
+//    },
+//                 columns : [
+//                  { "data" : "membername"},
+//                  { "data" : "username"},
+//                  { "data" : "email"},
+//                  { "data" : "levelid"},
+//                  { "data" : "departmentid"},
+//                  { "data" : "departmentid"},
+//                  { "data" : "isadmin"},
+//                  { "data" : "isactive"}                   
+//                 ]
+//            });    
+//});
+//    $("#userManagerDT").dataTable({lengthChange: false});
+//    $("#userManagerDT").DataTable({ responsive: true});
+//    $.ajax({
+//        url: hsRestUrl() + "/user",
+//        type: 'get',
+//        dataType: 'json',
+//        success: function (result) {
+//             daftar = result;      
+//            console.log(daftar);
+//        }
+//    });
