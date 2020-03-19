d6f66de2998582fbd374115c4e45bb3f76b9b298
untung bagus purnomo <untungbaguspurnomo30>
2020-03-19 23:19:14 +0700

    Add activate,list and findBy ProvinceDatabaseHelper

--- src/main/java/id/io/asset/util/database/ProvinceDatabaseHelper.java
+++ src/main/java/id/io/asset/util/database/ProvinceDatabaseHelper.java
@@ -8,6 +8,8 @@ package id.io.asset.util.database;
 import id.io.asset.model.ProvinceModel;
 import id.io.asset.util.constant.ConstantHelper;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import org.jdbi.v3.core.Handle;
 
 /**
@@ -32,7 +34,7 @@ public class ProvinceDatabaseHelper extends BaseDatabaseHelper {
                     .bind(ConstantHelper.PROVINCE_PROVINCEID, province.getProvinceid())
                     .bind(ConstantHelper.PROVINCE_PROVINCECODE, province.getProvincecode())
                     .bind(ConstantHelper.PROVINCE_PROVINCENAME, province.getProviincename())
-                    .bind(ConstantHelper.DEPARTMENT_ISACTIVE, false).execute();
+                    .bind(ConstantHelper.PROVINCE__ISACTIVE, false).execute();
 
         } catch (SQLException ex) {
             log.error(ProvinceDatabaseHelper.class.getName(), " - errorCreateProvince " + ex);
@@ -72,5 +74,56 @@ public class ProvinceDatabaseHelper extends BaseDatabaseHelper {
             log.error(ProvinceDatabaseHelper.class.getName(), " - errorDeleteProvince " + ex);
         }
         return result;
+    }
+    
+    //findById
+    public ProvinceModel findById(String provinceId) {
+        ProvinceModel province = new ProvinceModel();
+
+        log.debug(MemberLevelDatabaseHelper.class.getName(), " - getListUserLevel");
+
+        final String sql = "SELECT provinceid, provincecode, provincename, IF(isactive, 'true', 'false') isactive FROM masterprovince WHERE provinceid = :provinceid;";
+
+        try (Handle h = getHandle()) {
+            province = h.createQuery(sql).bind(ConstantHelper.PROVINCE_PROVINCEID, provinceId).mapToBean(ProvinceModel.class).first();
+        } catch (Exception ex) {
+            log.error(MemberLevelDatabaseHelper.class.getName(), " - errorGetListUserLevel " + ex);
+        }
+        return province;
+
+    }
+    
+    //list
+    public List<ProvinceModel> getList() {
+        List<ProvinceModel> provinceList = new ArrayList<>();
+
+        log.debug(MemberLevelDatabaseHelper.class.getName(), " - getListProvince");
+
+        final String sql = "SELECT provinceid, provincecode, provincename, IF(isactive, 'true', 'false') isactive FROM masterprovince;";
+
+        try (Handle h = getHandle()) {
+            provinceList = h.createQuery(sql).mapToBean(ProvinceModel.class).list();
+        } catch (Exception ex) {
+            log.error(MemberLevelDatabaseHelper.class.getName(), " - errorGetListProvince " + ex);
+        }
+        return provinceList;
+    }
+    
+    //activate
+    public int activate(String provinceId, boolean isActive) {
+        log.debug(ProvinceDatabaseHelper.class.getName(), "- activateProvince");
+
+        final String sql = "UPDATE masterprovince SET isactive = :isactive WHERE provinceid = :provinceid ;";
+        int row = 0;
+        try (Handle handle = getHandle()) {
+
+            row = handle.createUpdate(sql)
+                    .bind(ConstantHelper.PROVINCE_PROVINCEID, provinceId)
+                    .bind(ConstantHelper.PROVINCE__ISACTIVE, isActive).execute();
+
+        } catch (SQLException ex) {
+            log.error(ProvinceDatabaseHelper.class.getName(), " - errorActivateProvince " + ex);
+        }
+        return row;
     }
 }
