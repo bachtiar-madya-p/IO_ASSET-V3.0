d16712e89bb7b228ed3455034778b163b8e0d101
untung bagus purnomo <untungbaguspurnomo30>
2020-03-20 00:55:17 +0700

    Add create,update,list in ProvinceService

---
+++ src/main/java/id/io/asset/service/ProvinceService.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.io.asset.service;
+
+import id.io.asset.controller.ProvinceController;
+import id.io.asset.util.constant.ConstantHelper;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.apache.http.HttpStatus;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author user
+ */
+public class ProvinceService extends BaseService {
+    
+    private ProvinceController provinceController;
+    
+    public ProvinceService (){
+        this.provinceController = new ProvinceController();
+    }
+    
+    @GET
+    public Response provinceList(){
+        return Response.ok(provinceController.provinceList()).build();
+    }
+    
+    @GET
+    @Path("/{provinceId}")
+    public Response provinceById(@PathParam("provinceId") String provinceId){
+        return Response.ok(provinceController.getProvince(provinceId)).build();
+    }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response create(String jsonRequest){
+        JSONObject response = new JSONObject();
+        try{
+            response = provinceController.create(new JSONObject(jsonRequest));
+        }catch (JSONException ex){
+            response.put(ConstantHelper.HTTP_CODE, HttpStatus.SC_INTERNAL_SERVER_ERROR);
+            response.put(ConstantHelper.HTTP_REASON, "error_create_province");
+            response.put(ConstantHelper.HTTP_MESSAGE, "Error Create Province cause :" + ex.getMessage());
+        }
+        return Response.status((!response.has(ConstantHelper.HTTP_CODE))
+                ? HttpStatus.SC_OK : response.getInt(ConstantHelper.HTTP_CODE)).entity(response.toString()).build();
+    }
+    
+    @PUT
+    @Path("/{provinceId}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response update(@PathParam("provinceId") String provinceId, String jsonRequest) {
+        JSONObject response = new JSONObject();
+        try {
+            return Response.ok(provinceController.update(provinceId, new JSONObject(jsonRequest)).toString()).build();
+        } catch (JSONException ex) {
+            response.put(ConstantHelper.HTTP_CODE, HttpStatus.SC_INTERNAL_SERVER_ERROR);
+            response.put(ConstantHelper.HTTP_REASON, "error_update_province");
+            response.put(ConstantHelper.HTTP_MESSAGE, "Error Update Province cause :" + ex.getMessage());
+
+            return Response.status((!response.has(ConstantHelper.HTTP_CODE))
+                    ? HttpStatus.SC_OK : response.getInt(ConstantHelper.HTTP_CODE)).entity(response.toString()).build();
+        }
+
+    }
+    
+    
+}
