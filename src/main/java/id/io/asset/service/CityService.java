1282839eea844a0dd1344d8e941f65b51b72fc9e
untung bagus purnomo <untungbaguspurnomo30>
2020-03-22 00:41:09 +0700

    ChangeLicenseHeader CityService

--- src/main/java/id/io/asset/service/CityService.java
+++ src/main/java/id/io/asset/service/CityService.java
@@ -1,8 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/**
+  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+  *
+  * Copyright (c) 2019 IO-Teknologi Indonesia, and individual contributors
+  * as indicated by the @author tags. All Rights Reserved
+  *
+  * The contents of this file are subject to the terms of the
+  * Common Development and Distribution License (the License).
+  *
+  * Everyone is permitted to copy and distribute verbatim copies
+  * of this license document, but changing it is not allowed.
+  *
+  */
 package id.io.asset.service;
 
 import id.io.asset.controller.CityController;
@@ -22,11 +30,6 @@ import javax.ws.rs.core.Response;
 import org.apache.http.HttpStatus;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-/**
- *
- * @author user
- */
 
 @Path("city")
 @Produces(MediaType.APPLICATION_JSON)
@@ -70,7 +73,6 @@ public class CityService extends BaseService {
                     ? HttpStatus.SC_OK : response.getInt(ConstantHelper.HTTP_CODE)).entity(response.toString()).build();
         }
     }
-    
     
     //delete
     @DELETE
