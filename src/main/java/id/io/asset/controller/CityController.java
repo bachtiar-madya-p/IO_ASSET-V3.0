792b7a553cafb4760ba7a596a0c55e31f7434555
untung bagus purnomo <untungbaguspurnomo30>
2020-03-22 00:35:46 +0700

    add Update,delete,list,activate,findById CityDatabaseController

--- src/main/java/id/io/asset/controller/CityController.java
+++ src/main/java/id/io/asset/controller/CityController.java
@@ -50,5 +50,66 @@ public class CityController extends BaseController {
         return response;
     }
     
+    //delete
+    public JSONObject delete(String cityId) {
+        JSONObject json = new JSONObject();
+        int result = citydatabasehelper.delete(cityId);
+        if (result == 1) {
+            json.put(ConstantHelper.HTTP_CODE, HttpStatus.SC_OK);
+            json.put(ConstantHelper.HTTP_REASON, "delete_city_successful");
+            json.put(ConstantHelper.HTTP_MESSAGE, "Delete City Successful!");
+        } else {
+            json.put(ConstantHelper.HTTP_CODE, HttpStatus.SC_BAD_REQUEST);
+            json.put(ConstantHelper.HTTP_REASON, "error_delete_city");
+            json.put(ConstantHelper.HTTP_MESSAGE, "Error Delete City");
+        }
+        return json;
+    }
+    
+    //update
+    public JSONObject update(String cityId, JSONObject json) {
+        JSONObject response = new JSONObject();
+        if (json.length() != 0) {
+            CityModel model = new CityModel();
+            model.setCityname(json.getString(ConstantHelper.CITY_CITYNAME));
+            model.setProvinceid(json.getString(ConstantHelper.CITY_PROVINCEID));
+            citydatabasehelper.update(cityId, model);
+            response.put(ConstantHelper.HTTP_CODE, HttpStatus.SC_OK);
+            response.put(ConstantHelper.HTTP_REASON, "update_city_successful");
+            response.put(ConstantHelper.HTTP_MESSAGE, "Update City Successful!");
+        } else {
+            response.put(ConstantHelper.HTTP_CODE, HttpStatus.SC_BAD_REQUEST);
+            response.put(ConstantHelper.HTTP_REASON, "error_update_city");
+            response.put(ConstantHelper.HTTP_MESSAGE, "Error Update City : No such City");
+        }
+        return response;
+    }
+    
+    public CityModel getCity(String cityId) {
+        CityModel city = citydatabasehelper.findById(cityId);
+        return city;
+    }
+
+    //List
+    public List<CityModel> cityList() {
+        List<CityModel> cityList = citydatabasehelper.getList();
+        return cityList;
+    }
+    
+    //activate
+    public JSONObject activate(String cityId, JSONObject json) {
+        JSONObject response = new JSONObject();
+        if (json.length() != 0) {
+            citydatabasehelper.activate(cityId, json.getBoolean(ConstantHelper.CITY_ISACTIVE));
+            response.put(ConstantHelper.HTTP_CODE, HttpStatus.SC_OK);
+            response.put(ConstantHelper.HTTP_REASON, "activate/inactivate_city_successful");
+            response.put(ConstantHelper.HTTP_MESSAGE, "Activate/Inactivate City Successful!");
+        } else {
+            response.put(ConstantHelper.HTTP_CODE, HttpStatus.SC_BAD_REQUEST);
+            response.put(ConstantHelper.HTTP_REASON, "error_activate/inactivate_city");
+            response.put(ConstantHelper.HTTP_MESSAGE, "Error Activate/Inactivate City : No such City");
+        }
+        return response;
+    }
 }
 
