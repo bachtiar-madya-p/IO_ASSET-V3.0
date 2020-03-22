e7c7f2cb8b9120f7a6419a5af9b1651ba6edf41f
untung bagus purnomo <untungbaguspurnomo30>
2020-03-22 21:22:40 +0700

    add update,delete BuildingController

--- src/main/java/id/io/asset/controller/BuildingController.java
+++ src/main/java/id/io/asset/controller/BuildingController.java
@@ -49,4 +49,40 @@ public class BuildingController extends BaseController {
         }
         return response;
     }
+    
+    //delete
+    public JSONObject delete(String buildingId) {
+        JSONObject json = new JSONObject();
+        int result = buildingDatabaseHelper.delete(buildingId);
+        if (result == 1) {
+            json.put(ConstantHelper.HTTP_CODE, HttpStatus.SC_OK);
+            json.put(ConstantHelper.HTTP_REASON, "delete_building_successful");
+            json.put(ConstantHelper.HTTP_MESSAGE, "Delete Building Successful!");
+        } else {
+            json.put(ConstantHelper.HTTP_CODE, HttpStatus.SC_BAD_REQUEST);
+            json.put(ConstantHelper.HTTP_REASON, "error_delete_building");
+            json.put(ConstantHelper.HTTP_MESSAGE, "Error Delete Building");
+        }
+        return json;
+    }
+    
+    //update
+    public JSONObject update(String buildingId, JSONObject json) {
+        JSONObject response = new JSONObject();
+        if (json.length() != 0) {
+            BuildingModel model = new BuildingModel();
+            model.setBbuildingname(json.getString(ConstantHelper.BUILDING_BUILDINGNAME));
+            model.setDescription(json.getString(ConstantHelper.BUILDING_DESCRIPTION));
+            model.setCityid(json.getString(ConstantHelper.BUILDING_CITYID));
+            buildingDatabaseHelper.update(buildingId, model);
+            response.put(ConstantHelper.HTTP_CODE, HttpStatus.SC_OK);
+            response.put(ConstantHelper.HTTP_REASON, "update_building_successful");
+            response.put(ConstantHelper.HTTP_MESSAGE, "Update Building Successful!");
+        } else {
+            response.put(ConstantHelper.HTTP_CODE, HttpStatus.SC_BAD_REQUEST);
+            response.put(ConstantHelper.HTTP_REASON, "error_update_building");
+            response.put(ConstantHelper.HTTP_MESSAGE, "Error Update Building : No such Building");
+        }
+        return response;
+    }
 }
