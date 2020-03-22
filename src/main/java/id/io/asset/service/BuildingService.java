62d062bf84d8398c820ebb859d8c1dff625c5974
untung bagus purnomo <untungbaguspurnomo30>
2020-03-22 21:22:59 +0700

    add update,delete BuildingService

--- src/main/java/id/io/asset/service/BuildingService.java
+++ src/main/java/id/io/asset/service/BuildingService.java
@@ -8,8 +8,11 @@ package id.io.asset.service;
 import id.io.asset.controller.BuildingController;
 import id.io.asset.util.constant.ConstantHelper;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -45,5 +48,32 @@ public class BuildingService extends BaseService {
         }
         return Response.status((!response.has(ConstantHelper.HTTP_CODE))
                 ? HttpStatus.SC_OK : response.getInt(ConstantHelper.HTTP_CODE)).entity(response.toString()).build();
+    }
+    
+    //delete
+    @DELETE
+    @Path("/{buildingId}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response delete(@PathParam("buildingId") String buildingId) {
+        JSONObject response = buildingController.delete(buildingId);
+        return Response.status((!response.has(ConstantHelper.HTTP_CODE))
+                ? HttpStatus.SC_OK : response.getInt(ConstantHelper.HTTP_CODE)).entity(response.toString()).build();
+    }
+    
+    //update
+    @PUT
+    @Path("/{buildingId}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response update(@PathParam("buildingId") String buildingId, String jsonRequest) {
+        JSONObject response = new JSONObject();
+        try {
+            return Response.ok(buildingController.update(buildingId, new JSONObject(jsonRequest)).toString()).build();
+        } catch (JSONException ex) {
+            response.put(ConstantHelper.HTTP_CODE, HttpStatus.SC_INTERNAL_SERVER_ERROR);
+            response.put(ConstantHelper.HTTP_REASON, "error_update_building");
+            response.put(ConstantHelper.HTTP_MESSAGE, "Error Update Building cause :" + ex.getMessage());
+            return Response.status((!response.has(ConstantHelper.HTTP_CODE))
+                    ? HttpStatus.SC_OK : response.getInt(ConstantHelper.HTTP_CODE)).entity(response.toString()).build();
+        }
     }
 }
