a18c30336f26382d0eab63d9f4bd0e2ba92e871c
untung bagus purnomo <untungbaguspurnomo30>
2020-03-22 10:53:22 +0700

    add BuildingDatabaseHelper

---
+++ src/main/java/id/io/asset/util/database/BuildingDatabaseHelper.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.io.asset.util.database;
+
+/**
+ *
+ * @author user
+ */
+public class BuildingDatabaseHelper {
+    
+}
