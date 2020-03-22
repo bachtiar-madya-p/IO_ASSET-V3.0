2feffb1db85c8e3d9ace1c60b9d2e67b1ab4346c
untung bagus purnomo <untungbaguspurnomo30>
2020-03-22 21:22:09 +0700

    add update,delete BuildingDatabaseHelper

--- src/main/java/id/io/asset/util/database/BuildingDatabaseHelper.java
+++ src/main/java/id/io/asset/util/database/BuildingDatabaseHelper.java
@@ -20,6 +20,7 @@ public class BuildingDatabaseHelper extends BaseDatabaseHelper{
         log = getLogger(this.getClass());
     }
     
+    //create
     public int create (BuildingModel building){
         log.debug(BuildingDatabaseHelper.class.getName(), "- createBuilding");
         final String sql = "INSERT INTO masterbuilding (buildingid, buildingname, description, cityid, isactive) VALUES (:buildingid, :buildingname, :description, :cityid, :isactive);";
@@ -37,4 +38,33 @@ public class BuildingDatabaseHelper extends BaseDatabaseHelper{
         return row;
     }
     
+    //delete
+    public int delete(String buildingId) {
+        log.debug(BuildingDatabaseHelper.class.getName(), "- deleteBuilding");
+        final String sql = "DELETE FROM masterbuilding WHERE buildingid = :buildingid;";
+        int result = 0;
+        try (Handle handle = getHandle()) {
+            result = handle.createUpdate(sql).bind(ConstantHelper.BUILDING_BUILDINGID, buildingId).execute();
+        } catch (SQLException ex) {
+            log.error(BuildingDatabaseHelper.class.getName(), " - errorDeleteBuilding" + ex);
+        }
+        return result;
+    }
+    
+    //update
+    public int update(String buildingId, BuildingModel building) {
+        log.debug(BuildingDatabaseHelper.class.getName(), "- updateBuilding");
+        final String sql = "UPDATE masterbuilding SET  buildingname= :buildingname, description= :description, cityid= :cityid WHERE buildingid= :buildingid;";
+        int row = 0;
+        try (Handle handle = getHandle()) {
+            row = handle.createUpdate(sql)
+                    .bind(ConstantHelper.BUILDING_BUILDINGID, buildingId)
+                    .bind(ConstantHelper.BUILDING_BUILDINGNAME, building.getBuildingname())
+                    .bind(ConstantHelper.BUILDING_DESCRIPTION, building.getDescription())
+                    .bind(ConstantHelper.BUILDING_CITYID, building.getCityid()).execute();
+        } catch (SQLException ex) {
+            log.error(BuildingDatabaseHelper.class.getName(), " - errorUpdateBuilding " + ex);
+        }
+        return row;
+    }
 }
