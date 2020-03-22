85e7e59fed4985bd7da1e18031d3c42f56d009f5
untung bagus purnomo <untungbaguspurnomo30>
2020-03-22 16:25:13 +0700

    add Response create BuildingService

--- src/main/java/id/io/asset/service/BuildingService.java
+++ src/main/java/id/io/asset/service/BuildingService.java
@@ -5,10 +5,45 @@
  */
 package id.io.asset.service;
 
+import id.io.asset.controller.BuildingController;
+import id.io.asset.util.constant.ConstantHelper;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.apache.http.HttpStatus;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  *
  * @author user
  */
-public class BuildingService {
+@Path("building")
+@Produces(MediaType.APPLICATION_JSON)
+public class BuildingService extends BaseService {
     
+    private BuildingController buildingController;
+    
+    public BuildingService (){
+        this.buildingController = new BuildingController();
+    }
+    
+    //create
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response create (String jsonRequest) {
+        JSONObject response = new JSONObject();
+        try {
+            response = buildingController.create(new JSONObject(jsonRequest));
+        }catch (JSONException ex){
+            response.put(ConstantHelper.HTTP_STATUS_CODE, HttpStatus.SC_INTERNAL_SERVER_ERROR);
+            response.put(ConstantHelper.HTTP_REASON, "error_create_building");
+            response.put(ConstantHelper.HTTP_MESSAGE, "Error Create Building Cause :" + ex.getMessage());
+        }
+        return Response.status((!response.has(ConstantHelper.HTTP_CODE))
+                ? HttpStatus.SC_OK : response.getInt(ConstantHelper.HTTP_CODE)).entity(response.toString()).build();
+    }
 }
