a0daa4ae14e226b6b5faddac514f0e950cfa8849
untung bagus purnomo <untungbaguspurnomo30>
2020-03-22 15:44:35 +0700

    add JSONObject create BuildingController

--- src/main/java/id/io/asset/controller/BuildingController.java
+++ src/main/java/id/io/asset/controller/BuildingController.java
@@ -5,10 +5,48 @@
  */
 package id.io.asset.controller;
 
+import id.io.asset.model.BuildingModel;
+import id.io.asset.util.constant.ConstantHelper;
+import id.io.asset.util.database.BuildingDatabaseHelper;
+import id.io.asset.util.database.UUIDGeneratorHelper;
+import java.util.UUID;
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
+
 /**
  *
  * @author user
  */
-public class BuildingController {
+public class BuildingController extends BaseController {
     
+    private BuildingDatabaseHelper buildingDatabaseHelper;
+    private UUIDGeneratorHelper uuidGenerator;
+    
+    public BuildingController (){
+        buildingDatabaseHelper = new BuildingDatabaseHelper();
+        this.uuidGenerator = new UUIDGeneratorHelper();
+    }
+    
+    //create
+    public JSONObject create (JSONObject json) {
+        
+        JSONObject response = new JSONObject();
+        if(json.length() != 0){
+            UUID uuid = uuidGenerator.generateUUID(json.getString(ConstantHelper.BUILDING_BUILDINGNAME));
+            BuildingModel model = new BuildingModel();
+            model.setBuildingid(uuid.toString());
+            model.setBbuildingname(json.getString(ConstantHelper.BUILDING_BUILDINGNAME));
+            model.setDescription(json.getString(ConstantHelper.BUILDING_DESCRIPTION));
+            model.setCityid(json.getString(ConstantHelper.BUILDING_CITYID));
+            buildingDatabaseHelper.create(model);
+            response.put(ConstantHelper.HTTP_CODE, HttpStatus.SC_OK);
+            response.put(ConstantHelper.HTTP_REASON, "create_bulding_successful");
+            response.put(ConstantHelper.HTTP_MESSAGE, "Create Building Successful!");
+        } else {
+            response.put(ConstantHelper.HTTP_CODE, HttpStatus.SC_BAD_REQUEST);
+            response.put(ConstantHelper.HTTP_REASON, "error_create_building");
+            response.put(ConstantHelper.HTTP_MESSAGE, "Error Create Building : No such Building");
+        }
+        return response;
+    }
 }
