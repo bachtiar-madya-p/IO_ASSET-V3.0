4a0436626be3e86252cc61a0f8b86abc06c77855
tata cantika <tatacantika95@gmail.com>
2020-04-08 12:27:59 +0700

    Fix asset-resister.js

--- src/main/webapp/js/asset-register.js
+++ src/main/webapp/js/asset-register.js
@@ -31,8 +31,126 @@ $(document).ready(function () {
         });
 
         $('#assetRegisterDT').append(tab_data).DataTable({responsive: true});
+        
+        /* Create new Item */
+        $(".crud-submit").click(function (e) {
+            e.preventDefault();
+            var act = $("#create-item").find("form").attr("action", );
+
+            var location = $("#create-item").find("input[name='location']").val();
+            var assetcode = $("#create-item").find("input[name='assetcode']").val();
+            var buildingname = $("#create-item").find("input[name='buildingname']").val();
+            var membercode = $("#create-item").find("input[name='membercode']").val();
+            var rateid = $("#create-item").find("input[name='rateid']").val();
+            var geolocation = $("#create-item").find("input[name='geolocation']").val();
+            var photo = $("#create-item").find("input[name='photo']").val();
+            var note = $("#create-item").find("input[name='note']").val();
+            var tada = {
+                "location": location,
+                "assetcode": assetcode,
+                "buildingname": buildingname,
+                "membercode": membercode,
+                "rateid": rateid,
+                "geolocation": geolocation,
+                "photo": photo,
+                "note": note};
+            $.ajax({
+                dataType: 'json',
+                type: 'POST',
+                url: hsRestUrl() + "/user",
+                contentType: 'application/json; charset=utf-8',
+                data: JSON.stringify(tada)
+            }).done(function (data) {
+               window.parent.location = window.parent.location.href;
+
+            });
+        });
+
+        $('body').on("click", ".edit-item", function () {
+            var userId = $(this).parent("td").prev("td").prev("td").prev("td").prev("td").prev("td").prev("td").prev("td").text();
+            var location = $(this).parent("td").prev("td").prev("td").prev("td").prev("td").prev("td").prev("td").text();
+            var buildingname = $(this).parent("td").prev("td").prev("td").prev("td").prev("td").prev("td").text();
+            var geolocation = $(this).parent("td").prev("td").prev("td").prev("td").prev("td").text();
+            var photo = $(this).parent("td").prev("td").prev("td").prev("td").prev("td").text();
+            var note = $(this).parent("td").prev("td").prev("td").prev("td").prev("td").text();
+
+
+            $("#edit-item").find("input[name='membername']").val(location);
+            $("#edit-item").find("input[name='username']").val(buildingname);
+            $("#edit-item").find("input[name='email']").val(geolocation);
+            $("#edit-item").find("input[name='levelid']").val(photo);
+            $("#edit-item").find("input[name='departmentid']").val(note);
+
+            $("#edit-item").find("form").attr("action", hsRestUrl() + "/user/" + userId);
+
+            /* Updated new Item */
+            $(".crud-submit-edit").click(function (e) {
+
+
+                e.preventDefault();
+
+
+                var form_action = $("#edit-item").find("form").attr("action");
+                var location = $("#edit-item").find("input[name='location']").val();
+                var buildingname = $("#edit-item").find("input[name='buildingname']").val();
+                var geolocation = $("#edit-item").find("input[name='geolocation']").val();
+                var photo = $("#edit-item").find("input[name='photo']").val();
+                var note = $("#edit-item").find("input[name='note']").val();
+                var tada = {"location": location};
+                $.ajax({
+
+                    dataType: 'json',
+                    type: 'put',
+                    url: form_action,
+                    contentType: 'application/json; charset=utf-8',
+                    data: JSON.stringify(tada)
+
+
+                }).done(function (data) {
+
+                    window.parent.location = window.parent.location.href;
+                });
+            });
+        });
+        /* Remove Item */
+        $("body").on("click", ".remove-item", function () {
+
+            var userId = $(this).parent("td").prev("td").prev("td").prev("td").prev("td").prev("td").prev("td").prev("td").text();          
+
+            $.ajax({
+                dataType: 'json',
+                type: 'delete',
+                url: hsRestUrl() + "/user/" + userId,
+                contentType: 'application/json; charset=utf-8'
+            }).done(function (data) {
+
+              window.parent.location = window.parent.location.href;
+
+            });
+        });
+        $('body').on('click', ".deactive-user", function () {
+
+            if($(this).parent("td").prev("td").text()=== 'Inactive'){
+                var status = true;
+            }
+            else{
+                var status = false;
+            }
             
 
+            var userid = $(this).parent("td").prev("td").prev("td").prev("td").prev("td").prev("td").prev("td").prev("td").text();
+
+            var tada = {"isactive": status, "userid": userid};
+            $.ajax({
+                type: "post",
+                dataType: "json",
+                url: hsRestUrl() + "/user/activate/" + userid,
+
+                data: JSON.stringify(tada),
+                success: function (data) {
+                    window.parent.location = window.parent.location.href;
+                }
+            });
+        });
     });
 });
-     
